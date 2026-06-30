0df9b0b865cb408e2b21d1d7999ae39d8a4acff8
developerdevanshh <vermadev8749@gmail.com>
2026-06-30 17:49:06 +0530

    LambdaExpression Practice

--- src/main/java/LambdaExpression.java
+++ src/main/java/LambdaExpression.java
@@ -7,9 +7,16 @@ public class LambdaExpression {
         MathOperation add = (a,b) -> a + b;
         MathOperation subtract = (a,b) -> a - b;
         MathOperation multiply = (a,b) -> a * b;
+        MathOperation division = (a,b) -> a / b;
 
         System.out.println("Add: " + add.operate(10,5));
         System.out.println("Subtract: " + subtract.operate(10,2));
         System.out.println("Multiply: " + multiply.operate(15,8));
+        System.out.println("Divide: " + division.operate(20,4));
+        try{
+            System.out.println("Divide: " + division.operate(20,0));
+        }catch (ArithmeticException e){
+            return;
+        }
     }
 }
