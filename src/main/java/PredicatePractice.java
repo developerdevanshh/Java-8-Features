3ad51cb9e9e2424c1d347b13c1bd53a37fc4454c
developerdevanshh <vermadev8749@gmail.com>
2026-06-30 17:54:51 +0530

    PredicatePractice Practice

--- src/main/java/PredicatePractice.java
+++ src/main/java/PredicatePractice.java
@@ -2,14 +2,26 @@ import java.util.function.Predicate;
 
 public class PredicatePractice {
     public static void main(String[] args) {
-        Predicate<Integer> isEven = n -> n % 2 == 0;
-        Predicate<Integer> isPositive = n -> n > 0;
+//        Predicate<Integer> isEven = n -> n % 2 == 0;
+//        Predicate<Integer> isPositive = n -> n > 0;
+//
+//        Predicate<Integer> isEvenAndPositive = isEven.and(isPositive);
+//        Predicate<Integer> isOdd = isEven.negate();
+//
+//        System.out.println(isEven.test(8));
+//        System.out.println(isEvenAndPositive.test(103));
+//        System.out.println(isOdd.test(33));
 
-        Predicate<Integer> isEvenAndPositive = isEven.and(isPositive);
-        Predicate<Integer> isOdd = isEven.negate();
 
-        System.out.println(isEven.test(8));
-        System.out.println(isEvenAndPositive.test(103));
-        System.out.println(isOdd.test(33));
+        // A Predicate that checks if a String is a palindrome.
+        Predicate<String> isPalindrome = str -> {
+            String reversed = new StringBuilder(str).reverse().toString();
+            return str.equals(reversed);
+        };
+
+        System.out.println(isPalindrome.test("madam"));
+        System.out.println(isPalindrome.test("level"));
+        System.out.println(isPalindrome.test("java"));
+        System.out.println(isPalindrome.test("racecar"));
     }
 }
