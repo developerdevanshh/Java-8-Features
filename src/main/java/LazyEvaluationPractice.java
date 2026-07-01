34f6c35f6a9fcca4b2f7a352bf512f4f07c08c04
developerdevanshh <vermadev8749@gmail.com>
2026-07-01 18:44:58 +0530

    LazyEvaluationPractice

--- src/main/java/LazyEvaluationPractice.java
+++ src/main/java/LazyEvaluationPractice.java
@@ -1,14 +1,29 @@
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class LazyEvaluationPractice {
     public static void main(String[] args) {
+//        List<Integer> numbers = Arrays.asList(1,2,3,4,5);
+//
+//        System.out.println("Building Pipline....");
+//
+//        Optional<Integer> result = numbers.stream()
+//                .filter(n->{
+//                    System.out.println("Filtering: " + n);
+//                    return n>2;
+//                })
+//                .map(n->{
+//                    System.out.println("Mapping: " + n);
+//                    return n*n;
+//                })
+//                .findFirst(); // short-circuits - stops after first match
         List<Integer> numbers = Arrays.asList(1,2,3,4,5);
 
         System.out.println("Building Pipline....");
 
-        Optional<Integer> result = numbers.stream()
+        List<Integer> result = numbers.stream()
                 .filter(n->{
                     System.out.println("Filtering: " + n);
                     return n>2;
@@ -17,9 +32,9 @@ public class LazyEvaluationPractice {
                     System.out.println("Mapping: " + n);
                     return n*n;
                 })
-                .findFirst(); // short-circuits - stops after first match
+                .collect(Collectors.toList()); // short-circuits - stops after first match
 
 
-        System.out.println("Result: " + result.get());
+        System.out.println("Result: " + result);
     }
 }
