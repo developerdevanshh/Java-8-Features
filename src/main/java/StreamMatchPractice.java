8bf9aab9d6f3c71ca87bcdaefd73248ecfa3d561
developerdevanshh <vermadev8749@gmail.com>
2026-07-01 18:04:44 +0530

    Stream Practice

--- src/main/java/StreamMatchPractice.java
+++ src/main/java/StreamMatchPractice.java
@@ -4,14 +4,18 @@ import java.util.Optional;
 
 public class StreamMatchPractice {
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(10,15,20,33,42,51);
+//        List<Integer> numbers = Arrays.asList(10,15,20,33,42,51);
+//
+//        boolean allEven = numbers.stream().allMatch(n->n % 2 ==0);
+//        boolean anyOdd = numbers.stream().anyMatch(n->n % 2 !=0);
+//        Optional<Integer> firstOdd = numbers.stream().filter(n->n % 2 !=0).findFirst();
+//
+//        System.out.println("All even: " + allEven);
+//        System.out.println("Any odd: " + anyOdd);
+//        System.out.println("First odd: " + firstOdd.orElse(-1));
 
-        boolean allEven = numbers.stream().allMatch(n->n % 2 ==0);
-        boolean anyOdd = numbers.stream().anyMatch(n->n % 2 !=0);
-        Optional<Integer> firstOdd = numbers.stream().filter(n->n % 2 !=0).findFirst();
-
-        System.out.println("All even: " + allEven);
-        System.out.println("Any odd: " + anyOdd);
-        System.out.println("First odd: " + firstOdd.orElse(-1));
+        List<String> names = Arrays.asList("Rahul","Amit","Sara","Raj","Priya","Sam");
+        Boolean matchString = names.stream().allMatch("Dev");
+        System.out.println(matchString);
     }
 }
