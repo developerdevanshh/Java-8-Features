d0811e7071ef303132e3d482f5ed327edecc6cbb
developerdevanshh <vermadev8749@gmail.com>
2026-07-01 18:01:32 +0530

    Stream Practice

---
+++ src/main/java/StreamMatchPractice.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class StreamMatchPractice {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(10,15,20,33,42,51);
+
+        boolean allEven = numbers.stream().allMatch(n->n % 2 ==0);
+        boolean anyOdd = numbers.stream().anyMatch(n->n % 2 !=0);
+        Optional<Integer> firstOdd = numbers.stream().filter(n->n % 2 !=0).findFirst();
+
+        System.out.println("All even: " + allEven);
+        System.out.println("Any odd: " + anyOdd);
+        System.out.println("First odd: " + firstOdd.orElse(-1));
+    }
+}
