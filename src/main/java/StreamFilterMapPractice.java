a19f88eed9516fe4325a81a5cd6784a803883be3
developerdevanshh <vermadev8749@gmail.com>
2026-07-01 13:20:35 +0530

    Stream Practice

--- src/main/java/StreamFilterMapPractice.java
+++ src/main/java/StreamFilterMapPractice.java
@@ -3,13 +3,20 @@ import java.util.stream.*;
 
 public class StreamFilterMapPractice {
     public static void main(String[] args) {
-        List<String> names = Arrays.asList("Rahul","Amit","Sara","Raj","Rohan");
+//        List<String> names = Arrays.asList("Rahul","Amit","Sara","Raj","Rohan");
+//
+//        List<String> result = names.stream()
+//                .filter(name -> name.length() > 4)
+//                .map(String::toUpperCase)
+//                .collect(Collectors.toList());
+//
+//        System.out.println(result);
 
-        List<String> result = names.stream()
-                .filter(name -> name.length() > 4)
-                .map(String::toUpperCase)
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+
+        List<Integer> result = numbers.stream()
+                .filter(n -> n%2 ==0)
+                .map(n->n*n)
                 .collect(Collectors.toList());
-
-        System.out.println(result);
     }
 }
