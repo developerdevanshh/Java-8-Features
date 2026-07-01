55184d1154dfeda4fa388dd5a8c39c4cd0d33c1d
developerdevanshh <vermadev8749@gmail.com>
2026-07-01 13:35:50 +0530

    Stream Practice

--- src/main/java/StreamReducePractice.java
+++ src/main/java/StreamReducePractice.java
@@ -1,14 +1,21 @@
 import java.util.*;
 public class StreamReducePractice {
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1,2,3,4,5);
+//        List<Integer> numbers = Arrays.asList(1,2,3,4,5);
+//
+//        int sum = numbers.stream().reduce(0,Integer::sum);
+//        int product = numbers.stream().reduce(1,(a,b) -> a*b);
+//        Optional<Integer> max = numbers.stream().reduce((a,b) -> a>b ? a:b);
+//
+//        System.out.println("Sum: " + sum);
+//        System.out.println("Product: " + product);
+//        System.out.println("Max: " + max);
 
-        int sum = numbers.stream().reduce(0,Integer::sum);
-        int product = numbers.stream().reduce(1,(a,b) -> a*b);
-        Optional<Integer> max = numbers.stream().reduce((a,b) -> a>b ? a:b);
+        List<String> names = Arrays.asList("Rahul","Amit","Sara","Ravikant","Rohan");
 
-        System.out.println("Sum: " + sum);
-        System.out.println("Product: " + product);
-        System.out.println("Max: " + max);
+        Optional<String> result = names.stream()
+                .reduce((a,b) ->a.length() > b.length() ? a : b);
+
+        System.out.println(result);
     }
 }
