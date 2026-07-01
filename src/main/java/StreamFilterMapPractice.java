842d0822de3b4e07197eac3111d099701efdffb8
developerdevanshh <vermadev8749@gmail.com>
2026-07-01 13:02:32 +0530

    Stream Practice

---
+++ src/main/java/StreamFilterMapPractice.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.util.stream.*;
+
+public class StreamFilterMapPractice {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Rahul","Amit","Sara","Raj","Rohan");
+
+        List<String> result = names.stream()
+                .filter(name -> name.length() > 3)
+                .map(String::toUpperCase)
+                .collect(Collectors.toList());
+
+        System.out.println(result);
+    }
+}
